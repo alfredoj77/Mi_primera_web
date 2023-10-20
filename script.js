5909501cc4118e688f8c37c62669312861ea773a
alfredoj77 <alfredoj_ar@hotmail.com>
2023-10-19 21:15:00 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//alert("Hola este es mi Javascript");
+
+let nombre = "Alfredo";
+//nombre = "Daniel";
+
+var nombre1 = "Alfredo";
+
+const nombre2 = "Alfredo";
+//nombre2 = "María";
+
+console.log(nombre);
+
+console.log(nombre1);
+
+console.log(nombre2);
+
